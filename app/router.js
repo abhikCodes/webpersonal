01179839cd48d2d93c2881cb5b27b7aefb29f7a2
Abhik Sarkar <sarkarabhik97@gmail.com>
2018-06-09 20:37:59 +0530

    new

--- app/router.js
+++ app/router.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var helper = require('sendgrid').mail;
-var sg = require("sendgrid")("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");
-
+// var helper = require('sendgrid').mail;
+// var sg = require("sendgrid")("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");
+var mailgun = require("mailgun-js");
+var api_key = '39c8e70ac049b15b7fbaa13556b165be-b892f62e-07c76ef9';
+var DOMAIN = 'sandbox3e55e08ddc8045e89472658728735aad.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
 
 var router = express.Router();
 
@@ -30,29 +33,42 @@ router.post('/contact', urlencodedParser, function(req, res){
     console.log(req.body);
     console.log(req.body['name']);
 
-    var fromEmail = new helper.Email(req.body['email']);
-    var toEmail = new helper.Email("as242@snu.edu.in");
-    var subject = req.body['subject'];
-    var content = new helper.Content("text/plain", req.body['message']);
-    var mail = new helper.Mail(fromEmail, subject,toEmail, content);
+    var data = {
+      from: req.body['email'],
+      to: 'as242@snu.edu.in',
+      subject: req.body['subject'],
+      text: req.body['message']
+    };
 
-    var request = sg.emptyRequest({
-        method: "POST",
-        path: "/v3/mail/send",
-        body: mail.toJSON()
+    mailgun.messages().send(data, function (error, body) {
+        console.log("mailgun:");
+        console.log(body);
+        res.render('../mailresult');
     });
 
-    sg.API(request, function(error, response){
-        if(error){
-            console.log("Error");
-            res.render('../error');
-        }
-        console.log(response.statusCode);
-        console.log(response.body);
-        console.log(response.headers);
-        console.log("Succesfully sent");
-        res.render('../mailresult');
-    })
+    // var fromEmail = new helper.Email(req.body['email']);
+    // var toEmail = new helper.Email("as242@snu.edu.in");
+    // var subject = req.body['subject'];
+    // var content = new helper.Content("text/plain", req.body['message']);
+    // var mail = new helper.Mail(fromEmail, subject,toEmail, content);
+
+    // var request = sg.emptyRequest({
+    //     method: "POST",
+    //     path: "/v3/mail/send",
+    //     body: mail.toJSON()
+    // });
+    //
+    // sg.API(request, function(error, response){
+    //     if(error){
+    //         console.log("Error");
+    //         res.render('../error');
+    //     }
+    //     console.log(response.statusCode);
+    //     console.log(response.body);
+    //     console.log(response.headers);
+    //     console.log("Succesfully sent");
+    //     res.render('../mailresult');
+    // })
 
 })
 
